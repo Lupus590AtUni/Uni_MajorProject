7edd1909a5be649fdfe05d98ab02457ae196dbe5
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-02-09 11:12:59 +0000

    redo { and stuff

--- Assets/My Stuff/Scripts/GameController.cs
+++ Assets/My Stuff/Scripts/GameController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
+    void playerThinksTheyAreThere()
+    {
+
+    }
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if(Input.GetButton("Fire1"))
         {
             print("FIRE!");
--- Assets/My Stuff/Scripts/Navigator.cs
+++ Assets/My Stuff/Scripts/Navigator.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Navigator : MonoBehaviour {
+public class Navigator : MonoBehaviour
+{
     // https://docs.unity3d.com/Manual/nav-CreateNavMeshAgent.html
 
     public Transform goal;
+    public Transform gameController;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
         print("Navigator Script: started");
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
         //agent.path
     }
-	
+
+    void findLandmarks()
+    {
+
+    }
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
         
 	}
 }
