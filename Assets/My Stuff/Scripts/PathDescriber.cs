2ef2271a72445bc5c5b9ce1fd7aba672110b74dc
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-02-14 15:41:22 +0000

    blarg

--- Assets/My Stuff/Scripts/PathDescriber.cs
+++ Assets/My Stuff/Scripts/PathDescriber.cs
@@ -33,33 +33,32 @@ public class PathDescriber : MonoBehaviour
 
     private Landmark findNearestLandmark(Vector3 position)
     {
+        //TODO: Actually put stuff here
         return null;
     }
 
     private Vector3 findCornerHeading(Vector3 current, Vector3 next)
     {
+        //TODO: Actually put stuff here
         return new Vector3();
     }
 
     private customPath[] convertPathToCustom(NavMeshPath oldPath)
     {
+        //TODO: Actually put stuff here
         return null;
     }
 
     private string[] naturalLanguageConverter(customPath[] path)
     {
+        //TODO: Actually put stuff here
         //TODO: fancier natural language generation
         return null;
     }
 
     public string[] convertPath(NavMeshPath oldPath)
     {
+        //TODO: Actually put stuff here
         return null;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-		
-	}
 }
