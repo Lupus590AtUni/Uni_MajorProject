cd32c7f802910b32e81ef0a910fa24d68e4eca2a
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-02-16 14:51:24 +0000

    framework copied from master before the revert

--- Assets/My Stuff/Scripts/GuiController.cs
+++ Assets/My Stuff/Scripts/GuiController.cs
@@ -5,47 +5,60 @@ using UnityEngine;
 
 public class GuiController : MonoBehaviour
 {
+    [HideInInspector]
     public List<string> directions;
 
-    [SerializeField] private Rect directionsBox;
+    [SerializeField]
+    private Rect directionsBox;
+    [SerializeField]
+    private Vector2 pauseBoxSize;
+    private Rect PauseBox;
 
     private GameController gameController;
 
     // Use this for initialization
     void Start()
     {
-        print("GUI Script: started");
+        //print("GUI Script: started");
         gameController = FindObjectOfType<GameController>();
     }
-	
-	// Update is called once per frame
-	void Update()
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+
+    }
 
     void OnGUI()
     {
+
         switch(gameController.mode)
         {
             case GameController.Mode.game:
                 {
+
                     if(gameController.isPaused())
                     {
-                        //LOW: Pause Menu
-                        GUI.Box(new Rect(0, 0, directionsBox.size.x, directionsBox.size.y), "Paused");
+                        //LOW: Pause Menu temp code in place - doens't work
+                        PauseBox = new Rect((Screen.width - pauseBoxSize.x) / 2, (Screen.height - pauseBoxSize.y) / 2, pauseBoxSize.x, pauseBoxSize.y);
+                        GUI.BeginGroup(PauseBox);
+                        GUI.Box(PauseBox, "Paused");
+
+                        GUI.EndGroup();
                     }
                     else
                     {
+
                         // Normal Hud
+                        //LOW: improve
                         GUI.BeginGroup(directionsBox);
                         // Make a background box
-                        GUI.Box(new Rect(0, 0, directionsBox.size.x, directionsBox.size.y), "Directions");
+                        GUI.Box(directionsBox, "Directions");
 
                         int yPos = 20;
 
                         //print("directions.Count: "+ directions.Count);
-                        for(int i = 0; i < directions.Count; i++)
+                        for(int i = 0; i < directions.Length; i++)
                         {
                             GUI.Label(new Rect(5, yPos, directionsBox.size.x - 5, 1000), directions[i]);
                             yPos += 15;
@@ -58,11 +71,13 @@ public class GuiController : MonoBehaviour
             case GameController.Mode.menu:
                 {
                     //LOW: Main menu
+
                     break;
                 }
             case GameController.Mode.survey:
                 {
                     //LOW: Survey
+
                     break;
                 }
             default:
@@ -71,6 +86,6 @@ public class GuiController : MonoBehaviour
                     break;
                 }
         }
-        
+
     }
 }
