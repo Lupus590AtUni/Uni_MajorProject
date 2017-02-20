91497220cba34beb1c3f0d36e7c41d34f6601ca6
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-02-20 14:30:07 +0000

    moving this didn't help, but debug printouts look better now

--- Assets/My Stuff/Scripts/Navigator.cs
+++ Assets/My Stuff/Scripts/Navigator.cs
@@ -31,14 +31,18 @@ public class Navigator : MonoBehaviour
         agent.destination = gameController.currentGoal.position;
 
 
-        //HACK: or not I guess as this is the problem code
-        string[] str = pathDescriber.convertPathToString(agent.path);
+        
 
 
         print("agent.path.status: " + agent.path.status);
         print("agent.isPathStale: " + agent.isPathStale);
 
         print("agent.path.corners.Length: " + agent.path.corners.Length);
+
+        //HACK: or not I guess as this is the problem code
+        string[] str = pathDescriber.convertPathToString(agent.path);
+
+
         guiController.directions.Clear();
         for(int i = 0; i<agent.path.corners.Length; i++)
         {
