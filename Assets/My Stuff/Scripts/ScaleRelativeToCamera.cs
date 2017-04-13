4942db840f82dc9021e821cda496e5f1454180c1
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-04-13 14:53:31 +0100

    map marker scales nicely now

---
+++ Assets/My Stuff/Scripts/ScaleRelativeToCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//http://wiki.unity3d.com/index.php/CameraRelativeScale
+public class ScaleRelativeToCamera : MonoBehaviour
+{
+
+    public Camera cam;
+    public float objectScale = 1.0f;
+    private Vector3 initialScale;
+
+    // set the initial scale, and setup reference camera
+    void Start()
+    {
+        // record initial scale, use this as a basis
+        initialScale = transform.localScale;
+
+        // if no specific camera, grab the default camera
+        if(cam == null)
+            cam = Camera.main;
+    }
+
+    // scale object relative to distance from camera plane
+    void Update()
+    {
+        Plane plane = new Plane(cam.transform.forward, cam.transform.position);
+        float dist = plane.GetDistanceToPoint(transform.position);
+        transform.localScale = initialScale * dist * objectScale;
+    }
+}
